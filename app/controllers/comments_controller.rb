12d3e605aaf2bbbdd562a7f1a89312ba292baa87
rarapurpose <129743144+rarapurpose@users.noreply.github.com>
2023-04-23 20:58:18 +0900

    コメント表示

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -9,7 +9,7 @@ class CommentsController < ApplicationController
     else
      @prototype = @comment.prototype
      @comments = @prototype.comments
-      render "prototype/show"
+      render "prototypes/show"
     end  
   end
   
--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,13 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'prototypes#index'
-end
+
 
   resources :prototypes do
-    resources :comments, only: :create,
+    resources :comments, only: [:create, :show]
+    resources :users, only: :show
   end
    
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   
-
 end
- 
