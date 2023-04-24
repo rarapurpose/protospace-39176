66764a69f4854711f5328b00086abf6b50a37483
rarapurpose <129743144+rarapurpose@users.noreply.github.com>
2023-04-24 16:53:47 +0900

    ユーザー詳細ページ link_to

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,8 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @prototypes = current_user.prototypes
+    @prototype = Prototype.new
+    
   end
 
 end
